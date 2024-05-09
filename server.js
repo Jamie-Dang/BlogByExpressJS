db4a717e1e2670117941447552ff289a3720c528
Tuyen Dang <tuyendang878@gmail.com>
2024-05-09 15:50:56 +0700

    finish

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require("express");
-const path = require("path");
 const app = express();
 
 // Trả về static resource (image, css, js...) cho client => Đăng ký thư mục public
 app.use(express.static("public"));
 
-// Trả về một file html cho client
+// import path để làm việc với các đường dẫn tập tin và thư mục trên hệ thống tập tin của máy tính
+const path = require("path");
+// Trả về một file html cho client => Chuyển sang Trang chủ
+// Giờ cứ có một request tới trang chủ "/", thì trang home.html sẽ được trả về.
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "home.html"));
 });
 
-// This app starts a server and listens on port 5000 for connections
+// Trả về một file html cho client => Chuyển sang Trang Editor
+app.get("/editor", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "editor.html"));
+});
+
+// Ứng dụng này khởi động một máy chủ và lắng nghe trên cổng 3000 để chờ kết nối.
 const port = 3000;
 app.listen(port, () => {
   console.log("App listening on port 3000");
